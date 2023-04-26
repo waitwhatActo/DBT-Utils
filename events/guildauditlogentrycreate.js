303b2597399d9f7931c30335234058d95a2241cb
Acto <acton.clever@gmail.com>
2023-04-25 17:58:40 -0700

    Add Unban logging
    -Added Unban Logging
    -Fixed Some Bugs and moved some things around

--- events/guildauditlogentrycreate.js
+++ events/guildauditlogentrycreate.js
@@ -5,10 +5,10 @@ module.exports = {
 	name: Events.GuildAuditLogEntryCreate,
 	async execute(auditLog) {
 		const { action, executorId, targetId } = auditLog;
+		const executor = await bot.users.cache.get(executorId);
+		const target = await bot.users.cache.get(targetId);
 		switch (action) {
 		case AuditLogEvent.MemberBanAdd: {
-			const executor = await bot.users.cache.get(executorId);
-			const target = await bot.users.cache.get(targetId);
 			const embed = new EmbedBuilder()
 				.setAuthor({ name: target.tag, iconURL: target.displayAvatarURL() })
 				.setColor("#FF0000")
@@ -16,14 +16,31 @@ module.exports = {
 				.addFields([
 					{ name: "Responsible Moderator", value: `<@${executor.id}>`, inline: true },
 					{ name: "Reason", value: auditLog.reason ?? "No reason provided.", inline: true },
-					{ name: "Ban Time", value: `<t:${Math.round(auditLog.createdAt / 1000)}:F>`, inline: true },
+					{ name: "Ban Time", value: `<t:${Math.round(auditLog.createdAt / 1000)}:F>`, inline: false },
 					{ name: "Account Created", value: `<t:${Math.round(target.createdTimestamp / 1000)}:F>`, inline: true },
 					// { name: "Joined Server", value: `<t:${Math.round(target.joinedTimestamp / 1000)}:F>`, inline: true },
 				])
 				.setTimestamp()
 				.setThumbnail(target.displayAvatarURL() ?? "https://cdn.discordapp.com/embed/avatars/0.png?size=4096")
 				.setFooter({ text: `ID: ${target.id}`, iconURL: bot.user.displayAvatarURL() });
-				// @ts-expect-error
+				// @ts-expect-error 1
+			await (bot.channels.cache.get(ids.channels.mod.log)).send({ embeds: [embed] });
+			break;
+		}
+		case AuditLogEvent.MemberBanRemove: {
+			const embed = new EmbedBuilder()
+				.setAuthor({ name: target.tag, iconURL: target.displayAvatarURL() })
+				.setColor("#00FF00")
+				.setTitle("**Member Unbanned**")
+				.addFields([
+					{ name: "Responsible Moderator", value: `<@${executor.id}>`, inline: true },
+					{ name: "Unban Time", value: `<t:${Math.round(auditLog.createdAt / 1000)}:F>`, inline: false },
+					{ name: "Account Created", value: `<t:${Math.round(target.createdTimestamp / 1000)}:F>`, inline: true },
+				])
+				.setTimestamp()
+				.setThumbnail(target.displayAvatarURL() ?? "https://cdn.discordapp.com/embed/avatars/0.png?size=4096")
+				.setFooter({ text: `ID: ${target.id}`, iconURL: bot.user.displayAvatarURL() });
+			// @ts-expect-error 1
 			await (bot.channels.cache.get(ids.channels.mod.log)).send({ embeds: [embed] });
 		}
 		}
