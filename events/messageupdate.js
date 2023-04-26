2a2d3ce900341256d1f36830bd07f2a0ed37bcba
Acto <acton.clever@gmail.com>
2023-04-25 20:54:11 -0700

    Added MessageUpdate Logging

---
+++ events/messageupdate.js
@@ -0,0 +1,22 @@
+const { EmbedBuilder, Events } = require("discord.js");
+const { bot, ids } = require("../index.js");
+
+module.exports = {
+	name: Events.MessageUpdate,
+	async execute(oldMessage, newMessage) {
+		if (newMessage.content == oldMessage.content) return;
+		const embed = new EmbedBuilder()
+			.setAuthor({ name: newMessage.author.tag, iconURL: newMessage.author.displayAvatarURL(), url: `https://discord.com/users/${newMessage.author.id}` })
+			.setColor("Random")
+			.setTitle("**Message Edited**")
+			.setURL(newMessage.url)
+			.setDescription(`**Message sent by ${newMessage.author} edited in ${newMessage.channel}**`)
+			.addFields([
+				{ name: "Old Message", value: `${oldMessage.content ?? "No content"}`, inline: true },
+				{ name: "New Message", value: `${newMessage.content ?? "No content"}`, inline: true },
+			])
+			.setFooter({ text: `User ID: ${newMessage.author.id}`, iconURL: bot.user.displayAvatarURL() })
+			.setTimestamp();
+		await (oldMessage.guild.channels.cache.get(ids.channels.mod.log)).send({ embeds: [embed] });
+	},
+};
